9fc9bf9a68f916b613c592831f2d355690d09c68
misbashaikh2004 <misba6602@gmail.com>
2025-07-15 22:30:26 +0530

    First commit

---
+++ 11.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    float x, y;
+    printf("Enter x and y coordinates: ");
+    scanf("%f%f", &x, &y);
+
+    printf("X = %.2f, Y = %.2f\n", x, y);
+    return 0;
+}
