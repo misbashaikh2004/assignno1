e1315e2665522b7e1beec986e7cf9353f6f096e5
misbashaikh2004 <misba6602@gmail.com>
2025-07-15 10:44:11 +0530

    First main

---
+++ 4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+float u,a,v,t,s;
+    printf("Enter initial velocity,acceleration and time:");
+    scanf("%f %f %f",&u,&a,&t);
+    v=u+a*t;
+    s=u*t+0.5*a*t*t;
+    printf("Final velocity=%.2f\nDistance=%.2f\n",v,s);
+  return 0;
+
+}
+
+    
